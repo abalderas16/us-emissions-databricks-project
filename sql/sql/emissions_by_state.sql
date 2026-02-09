647665007a12dcbc2d6578b6b60ccb7192a2aecd
Arley Balderas <balderasa1228@gmail.com>
2026-02-09 16:52:52 -0600

    Delete sql/sql directory

--- sql/sql/emissions_by_state.sql
+++
@@ -1,5 +0,0 @@
-SELECT state_abbr,
-       SUM(CAST (replace(`GHG emissions mtons CO2e`, ',', '') AS DOUBLE)) AS emissions_totals
-FROM emissions_data
-GROUP BY state_abbr
-ORDER BY emissions_totals DESC
